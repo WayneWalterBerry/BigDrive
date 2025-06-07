3d249f71a07c63c3b94559903f57d4f8fa48e28b
Wayne Berry <wayneb@microsoft.com>
2025-06-07 05:48:15 -0700

    IBigDriveFileOperations

--- src/IShellFolder/BigDriveDropTarget.cpp
+++ src/IShellFolder/BigDriveDropTarget.cpp
@@ -46,37 +46,75 @@ BigDriveDropTarget::~BigDriveDropTarget()
 /// <returns>TRUE if the format is supported; FALSE otherwise.</returns>
 BOOL BigDriveDropTarget::IsFormatSupported(IDataObject* pDataObj)
 {
-    if (!pDataObj)
-        return FALSE;
+    BOOL bSupported = FALSE;
+    FORMATETC* pfmte = nullptr;
+    HRESULT hr = E_FAIL;
+    CLIPFORMAT cfShellIdList = 0;
+    CLIPFORMAT cfFileDescriptor = 0;
+    CLIPFORMAT cfFileContents = 0;
 
-    // Check for Shell IDList Array format
-    FORMATETC fmte = {
-        (CLIPFORMAT)RegisterClipboardFormat(CFSTR_SHELLIDLIST),
-        nullptr,
-        DVASPECT_CONTENT,
-        -1,
-        TYMED_HGLOBAL
-    };
-
-    if (SUCCEEDED(pDataObj->QueryGetData(&fmte)))
-        return TRUE;
-
-    // Check for standard file drop format (HDROP)
-    fmte.cfFormat = CF_HDROP;
-    if (SUCCEEDED(pDataObj->QueryGetData(&fmte)))
-        return TRUE;
-
-    // Check for FileGroupDescriptor format (used by virtual files)
-    fmte.cfFormat = (CLIPFORMAT)RegisterClipboardFormat(CFSTR_FILEDESCRIPTOR);
-    if (SUCCEEDED(pDataObj->QueryGetData(&fmte)))
+    if (pDataObj == nullptr)
     {
-        // Also need FileContents
-        fmte.cfFormat = (CLIPFORMAT)RegisterClipboardFormat(CFSTR_FILECONTENTS);
-        if (SUCCEEDED(pDataObj->QueryGetData(&fmte)))
-            return TRUE;
+        goto End;
     }
 
-    return FALSE;
+    pfmte = new (std::nothrow) FORMATETC;
+    if (pfmte == nullptr)
+    {
+        goto End;
+    }
+
+    pfmte->cfFormat = 0;
+    pfmte->ptd = nullptr;
+    pfmte->dwAspect = DVASPECT_CONTENT;
+    pfmte->lindex = -1;
+    pfmte->tymed = TYMED_HGLOBAL;
+
+    // Check for Shell IDList Array format
+    cfShellIdList = (CLIPFORMAT)RegisterClipboardFormat(CFSTR_SHELLIDLIST);
+    pfmte->cfFormat = cfShellIdList;
+    hr = pDataObj->QueryGetData(pfmte);
+    if (SUCCEEDED(hr))
+    {
+        bSupported = TRUE;
+        goto End;
+    }
+
+    // Check for standard file drop format (HDROP)
+    pfmte->cfFormat = CF_HDROP;
+    hr = pDataObj->QueryGetData(pfmte);
+    if (SUCCEEDED(hr))
+    {
+        bSupported = TRUE;
+        goto End;
+    }
+
+    // Check for FileGroupDescriptor format (used by virtual files)
+    cfFileDescriptor = (CLIPFORMAT)RegisterClipboardFormat(CFSTR_FILEDESCRIPTOR);
+    pfmte->cfFormat = cfFileDescriptor;
+    hr = pDataObj->QueryGetData(pfmte);
+    if (SUCCEEDED(hr))
+    {
+        // Also need FileContents
+        cfFileContents = (CLIPFORMAT)RegisterClipboardFormat(CFSTR_FILECONTENTS);
+        pfmte->cfFormat = cfFileContents;
+        hr = pDataObj->QueryGetData(pfmte);
+        if (SUCCEEDED(hr))
+        {
+            bSupported = TRUE;
+            goto End;
+        }
+    }
+
+End:
+
+    if (pfmte != nullptr)
+    {
+        delete pfmte;
+        pfmte = nullptr;
+    }
+
+    return bSupported;
 }
 
 /// <summary>
@@ -88,107 +126,141 @@ HRESULT BigDriveDropTarget::ProcessDrop(IDataObject* pDataObj)
 {
     HRESULT hr = E_FAIL;
     FORMATETC fmtec = { CF_HDROP, nullptr, DVASPECT_CONTENT, -1, TYMED_HGLOBAL };
-    STGMEDIUM stgmed;
+    STGMEDIUM stgmed = {};
     DriveConfiguration driveConfig;
     BigDriveInterfaceProvider* pProvider = nullptr;
     IBigDriveFileOperations* pFileOps = nullptr;
     PIDLIST_ABSOLUTE pidlFolder = nullptr;
     BSTR bstrTargetFolder = nullptr;
+    HDROP hDrop = nullptr;
+    CLSID driveGuid = GUID_NULL;
+    UINT fileCount = 0;
 
-    // First try to handle CF_HDROP (standard file drag-drop)
+    // Try to handle CF_HDROP (standard file drag-drop)
     fmtec.cfFormat = CF_HDROP;
-    if (SUCCEEDED(pDataObj->QueryGetData(&fmtec)))
-    {
-        hr = pDataObj->GetData(&fmtec, &stgmed);
-        if (SUCCEEDED(hr))
-        {
-            HDROP hDrop = static_cast<HDROP>(stgmed.hGlobal);
-            if (hDrop)
-            {
-                // Get number of files dropped
-                UINT fileCount = DragQueryFile(hDrop, 0xFFFFFFFF, nullptr, 0);
-                if (fileCount > 0)
-                {
-                    // Get target folder path
-                    hr = m_pFolder->GetCurFolder(&pidlFolder);
-                    if (FAILED(hr) || !pidlFolder)
-                    {
-                        ReleaseStgMedium(&stgmed);
-                        return hr;
-                    }
-
-                    hr = m_pFolder->GetPathForProviders(pidlFolder, bstrTargetFolder);
-                    if (FAILED(hr) || !bstrTargetFolder)
-                    {
-                        ILFree(pidlFolder);
-                        ReleaseStgMedium(&stgmed);
-                        return hr;
-                    }
-
-                    // Get the BigDrive interface 
-                    CLSID driveGuid;
-                    hr = m_pFolder->GetProviderCLSID(driveGuid);
-                    if (SUCCEEDED(hr))
-                    {
-                        hr = BigDriveConfigurationClient::GetDriveConfiguration(driveGuid, driveConfig);
-                        if (SUCCEEDED(hr))
-                        {
-                            pProvider = new BigDriveInterfaceProvider(driveConfig);
-                            if (pProvider)
-                            {
-                                hr = pProvider->GetIBigDriveFileOperations(&pFileOps);
-                                if (SUCCEEDED(hr) && pFileOps)
-                                {
-                                    // Process each dropped file
-                                    for (UINT i = 0; i < fileCount && SUCCEEDED(hr); i++)
-                                    {
-                                        WCHAR filePath[MAX_PATH];
-                                        UINT cch = DragQueryFile(hDrop, i, filePath, ARRAYSIZE(filePath));
-                                        if (cch > 0 && cch < MAX_PATH)
-                                        {
-                                            // Copy the file to BigDrive
-                                            hr = pFileOps->CopyFileToBigDrive(driveGuid, filePath, bstrTargetFolder);
-                                        }
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                hr = E_OUTOFMEMORY;
-                            }
-                        }
-                    }
-                }
-            }
-
-            ReleaseStgMedium(&stgmed);
-        }
-    }
-    else
+    if (FAILED(pDataObj->QueryGetData(&fmtec)))
     {
         // Try Shell IDList format
         fmtec.cfFormat = (CLIPFORMAT)RegisterClipboardFormat(CFSTR_SHELLIDLIST);
-        if (SUCCEEDED(pDataObj->QueryGetData(&fmtec)))
+        if (FAILED(pDataObj->QueryGetData(&fmtec)))
         {
-            // This would require more complex handling to extract PIDLs and process them
-            // For a complete implementation, we would extract the PIDLs, get their file paths,
-            // and use the BigDrive file operations interface to copy/move them
-            hr = E_NOTIMPL; // Not implemented in this example
+            hr = E_FAIL;
+            goto End;
+        }
+        // Not implemented
+        hr = E_NOTIMPL;
+        goto End;
+    }
+
+    hr = pDataObj->GetData(&fmtec, &stgmed);
+    if (FAILED(hr))
+    {
+        goto End;
+    }
+
+    hDrop = static_cast<HDROP>(stgmed.hGlobal);
+    if (!hDrop)
+    {
+        hr = E_FAIL;
+        goto End;
+    }
+
+    fileCount = DragQueryFile(hDrop, 0xFFFFFFFF, nullptr, 0);
+    if (fileCount == 0)
+    {
+        hr = E_FAIL;
+        goto End;
+    }
+
+    hr = m_pFolder->GetCurFolder(&pidlFolder);
+    if (FAILED(hr) || !pidlFolder)
+    {
+        goto End;
+    }
+
+    hr = m_pFolder->GetPathForProviders(pidlFolder, bstrTargetFolder);
+    if (FAILED(hr) || !bstrTargetFolder)
+    {
+        goto End;
+    }
+
+    hr = m_pFolder->GetProviderCLSID(driveGuid);
+    if (FAILED(hr))
+    {
+        goto End;
+    }
+
+    hr = BigDriveConfigurationClient::GetDriveConfiguration(driveGuid, driveConfig);
+    if (FAILED(hr))
+    {
+        goto End;
+    }
+
+    pProvider = new (std::nothrow) BigDriveInterfaceProvider(driveConfig);
+    if (pProvider == nullptr)
+    {
+        hr = E_OUTOFMEMORY;
+        goto End;
+    }
+
+    hr = pProvider->GetIBigDriveFileOperations(&pFileOps);
+    if (FAILED(hr) || pFileOps == nullptr)
+    {
+        goto End;
+    }
+
+    // Process each dropped file
+    for (UINT i = 0; i < fileCount && SUCCEEDED(hr); i++)
+    {
+        WCHAR filePath[MAX_PATH] = {};
+        UINT cch = DragQueryFile(hDrop, i, filePath, ARRAYSIZE(filePath));
+        if (cch > 0 && cch < MAX_PATH)
+        {
+            hr = pFileOps->CopyFileToBigDrive(driveGuid, filePath, bstrTargetFolder);
+            if (FAILED(hr))
+            {
+                goto End;
+            }
         }
     }
 
-    // Clean up resources
+End:
+
     if (pFileOps)
+    {
         pFileOps->Release();
+        pFileOps = nullptr;
+    }
 
     if (pProvider)
+    {
         delete pProvider;
+        pProvider = nullptr;
+    }
 
     if (bstrTargetFolder)
+    {
         SysFreeString(bstrTargetFolder);
+        bstrTargetFolder = nullptr;
+    }
 
     if (pidlFolder)
+    {
         ILFree(pidlFolder);
+        pidlFolder = nullptr;
+    }
+
+    if (stgmed.pUnkForRelease)
+    {
+        stgmed.pUnkForRelease->Release();
+        stgmed.pUnkForRelease = nullptr;
+    }
+
+    if (stgmed.hGlobal)
+    {
+        ReleaseStgMedium(&stgmed);
+        stgmed.hGlobal = nullptr;
+    }
 
     return hr;
 }
