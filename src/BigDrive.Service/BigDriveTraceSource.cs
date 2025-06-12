9ee3f7ad05eb8a2caad0ca3a624ad2fbcbc6025c
Wayne Berry <wayneb@microsoft.com>
2025-06-11 20:02:17 -0700

    Rename BigDrive.Service Folder

---
+++ src/BigDrive.Service/BigDriveTraceSource.cs
@@ -0,0 +1,50 @@
+﻿// <copyright file="BigDriveConfiguration.cs" company="Wayne Walter Berry">
+// Copyright (c) Wayne Walter Berry. All rights reserved.
+// </copyright>
+
+using System.Diagnostics;
+
+namespace BigDrive.Service
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class BigDriveTraceSource : TraceSource
+    {
+        private const string SourceName = "BigDrive.Service";
+
+        private static readonly Lazy<BigDriveTraceSource> _instance =
+            new Lazy<BigDriveTraceSource>(() => new BigDriveTraceSource());
+
+        public static BigDriveTraceSource Instance => _instance.Value;
+
+        static BigDriveTraceSource()
+        {
+            EventLogTraceListener eventLogListener = new EventLogTraceListener(SourceName);
+            Instance.Listeners.Add(eventLogListener);
+            Instance.Switch = new SourceSwitch("BigDriveSwitch", "All");
+
+            Instance.TraceInformation($"{SourceName} Trace Source Initialized");
+            Instance.Flush();
+        }
+
+        private BigDriveTraceSource() 
+            : base(SourceName)
+        {
+        }
+
+        /// <summary>
+        /// Trace a message with the specified event type and event ID.
+        /// </summary>
+        /// <param name="message">Message with Formatter</param>
+        /// <param name="args">Arguements to Formatter</param>
+        public void TraceError(string message, params object[] args)
+        {
+            TraceEvent(TraceEventType.Error, 0, message, args);
+        }
+    }
+}
