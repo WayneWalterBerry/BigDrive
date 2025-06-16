6c7c6a10163b8c34b21725b3dfd89a8b74c8fd4c
Wayne W Berry <waynewberry@outlook.com>
2025-06-16 17:15:51 +0000

    BigDrive.Extension

---
+++ src/BigDrive.Extension/LaunchDebugger.h
@@ -0,0 +1,10 @@
+// <copyright file="LaunchDebugger.h" company="Wayne Walter Berry">
+// Copyright (c) Wayne Walter Berry. All rights reserved.
+// </copyright>
+
+#include <windows.h>  
+
+#pragma once
+
+bool LaunchDebugger();
+
